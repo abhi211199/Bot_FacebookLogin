91ef7fce08c51c32dc212d48d5d805d5119d32d9
Abhishek Garain <abhi211199@gmail.com>
2018-05-25 18:17:36 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from getpass import getpass
+
+usr = input('Enter your username or E-mail or phone no.:')
+pwd = getpass('Enter your password: ')
+
+driver = webdriver.Chrome()
+driver.get('https://www.facebook.com/')
+
+username_box = driver.find_element_by_id('email')
+username_box.send_keys(usr)
+
+password_box = driver.find_element_by_id('pass')
+password_box.send_keys(pwd)
+
+login_btn = driver.find_element_by_id('u_0_3')
+login_btn.submit()
+
+
+dt=input('logout')
+if(dt=='Y'):
+    logout = driver.find_element_by_id('u_1p_3')
+    logout.submit()
